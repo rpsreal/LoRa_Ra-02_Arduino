8e72054918973b593fe7a4ea4917e5537a1e0534
Rui Silva <ruisilva.real@sapo.pt>
2018-05-16 20:01:57 +0100

    Update LORA_CLIENT.ino

--- LORA_CLIENT.ino
+++ LORA_CLIENT.ino
@@ -1,12 +1,9 @@
 // This program sends a response whenever it receives the "INF" mens
 //
-// Arduino9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Arduino9x_TX
+// Copyright 2018 Rui Silva.
+// This file is part of rpsreal/LoRa_Ra-02_Arduino
+// Based on example Arduino9x_RX RADIOHEAD library
+// It is designed to work with LORA_SERVER
 
 #include <SPI.h>
 #include <RH_RF95.h>
