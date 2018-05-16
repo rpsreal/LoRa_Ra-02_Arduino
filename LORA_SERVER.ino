e87604aea312215db3762129c433978359778d19
Rui Silva <ruisilva.real@sapo.pt>
2018-05-16 16:16:42 +0100

    Update and rename LoRa_Sender.ino to LORA_SERVER.ino

--- LORA_SERVER.ino
+++ LORA_SERVER.ino
@@ -1,3 +1,5 @@
+//  This program asks a client for data and waits for the response, then sends an ACK. 
+//
 // LoRa 9x_TX
 // -*- mode: C++ -*-
 // Example sketch showing how to create a simple messaging client (transmitter)
@@ -57,46 +59,43 @@ void setup()
   rf95.setTxPower(23, false);
 }
 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
+int8_t send_ack=0; // flag var
 
 void loop()
 {
-  Serial.println("Sending to rf95_server");
-  // Send a message to rf95_server
-  
-  char radiopacket[20] = "Hello World #      ";
-  itoa(packetnum++, radiopacket+13, 10);
-  Serial.print("Sending "); Serial.println(radiopacket);
-  radiopacket[19] = 0;
-  
-  Serial.println("Sending..."); delay(10);
-  rf95.send((uint8_t *)radiopacket, 20);
 
-  Serial.println("Waiting for packet to complete..."); delay(10);
-  rf95.waitPacketSent();
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+  while (send_ack==0){
+    digitalWrite(LED, HIGH);
+    Serial.println("Send: INF");
+    char radiopacket[3] = "INF";
+    rf95.send((uint8_t *)radiopacket, 3);
+    delay(10);
+    rf95.waitPacketSent();
 
-  Serial.println("Waiting for reply..."); delay(10);
-  if (rf95.waitAvailableTimeout(1000))
-  { 
-    // Should be a reply message for us now   
-    if (rf95.recv(buf, &len))
-   {
-      Serial.print("Got reply: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);    
-    }
-    else
-    {
-      Serial.println("Receive failed");
-    }
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.waitAvailableTimeout(1000)){ 
+      if (rf95.recv(buf, &len)){
+         Serial.print("Receive: ");
+         Serial.println((char*)buf);
+         send_ack=1;
+         //Serial.print("RSSI: ");
+         //Serial.println(rf95.lastRssi(), DEC);    
+      }else{
+         Serial.println("Receive failed");
+      }
+    }else{
+         Serial.println("Send INF again");
+   }
+  
+  if(send_ack==1){ //Send: ACK
+    Serial.println("Send: ACK");
+    char radiopacket[3] = "ACK";
+    rf95.send((uint8_t *)radiopacket, 3);
+    delay(10);
+    rf95.waitPacketSent();
+    send_ack=0;
   }
-  else
-  {
-    Serial.println("No reply, is there a listener around?");
-  }
-  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(10000);
 }
