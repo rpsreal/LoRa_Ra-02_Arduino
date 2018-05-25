0799f5fc2a74cbb6328c5b0c256555bd93b2639b
Rui Silva <ruisilva.real@sapo.pt>
2018-05-25 20:39:13 +0100

    Update LORA_SERVER_encrypted.ino

--- LORA_SERVER_encrypted.ino
+++ LORA_SERVER_encrypted.ino
@@ -3,7 +3,7 @@
 // Copyright 2018 Rui Silva.
 // This file is part of rpsreal/LoRa_Ra-02_Arduino
 // Based on example LoRa 9x_TX RADIOHEAD library
-// It is designed to work with LORA_CLIENT
+// It is designed to work with LORA_CLIENT_encrypted.ino
 
 #include <SPI.h>      // SPI
 #include <RH_RF95.h>  // HadioHead http://www.airspayce.com/mikem/arduino/RadioHead/
